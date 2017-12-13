2a87abbfe4ef9acd43da0da9e14c911f6dbc2d56
程瑞 <chengrui_tfc@163.com>
2017-12-13 20:01:29 +0800

    Add files via upload

---
+++ DAY1.py
@@ -0,0 +1,13 @@
+print("Hello World")
+print("hello again")
+print("I like typing this.")
+print("this is fun.")
+print('Yay! Printing.')
+print("i'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+print('hello World')
+print("hello world")
+#print('hello world')
+
+print("Hello, Cross Sea.               How are you?          Nice to meet you.")
