a5372db4b6fa0261195a95840e8ab12add50b440
程瑞 <chengrui_tfc@163.com>
2017-12-13 22:43:57 +0800

    Add files via upload

---
+++ ex03.py
@@ -0,0 +1,23 @@
+print("I will now count my chickens:") # Just print some words
+
+print("Hens", 25 + 30 / 6) #print words and do math
+print("Roosters", 100 - 25 * 3 % 4) # print words and do math “%” means find the remainder
+
+print("Now I will count the eggs:") # Just print words
+
+print (3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6) # Do math 这个表达式也没有加引号，然后程序就把它给运算出来了。
+
+print ("Is it true that 3 + 2 < 5 -7 ?") # Print words
+
+print (3 + 2 < 5 -7) #程序会判断表达式的真伪，然后给出答案，这句话没加引号，没加引号好像是表达式，是可以进行运算和判断的，那问题来了，我应该怎么写才能让它打印出 "5<-2" 这个表示式呢？
+
+print ("What is 3 + 2?", 3 +2) # 引号部分如实的被output出来了，没有引号的部分就会被运算or判断真伪  # 尤其有一点需要注意，不同的语句用逗号“,”隔开！！！！
+print ("What is 5 - 7?", 5 -7) # 引号部分如实的被output出来了，没有引号的部分就会被运算or判断真伪
+
+print ("Oh, that's why it's false.") # 打印字符
+
+print ("How about some more.") # 打印字符
+
+print ("Is it greater?", 5 > -2) # 引号部分如实的被output出来了，没有引号的部分就会被运算or判断真伪
+print ("Is it greater or equal?", 5 >= -2) # 引号部分如实的被output出来了，没有引号的部分就会被运算or判断真伪
+print ("Is it less or equal?", 5 <= -2) # 引号部分如实的被output出来了，没有引号的部分就会被运算or判断真伪
---
+++ ex03_drill_3.py
@@ -0,0 +1,1 @@
+print ("How many days left are there before Spring Festival?", 31 -13 +31+13)
