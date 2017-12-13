a6b7609cd48312cbf85206b5838be1c231378b3f
程瑞 <chengrui_tfc@163.com>
2017-12-13 20:03:02 +0800

    Update DAY1.py

--- DAY1.py
+++ DAY1.py
@@ -2,7 +2,7 @@ print("Hello World")
 print("hello again")
 print("I like typing this.")
 print("this is fun.")
-print('Yay! Printing.')
+print('Yay! Printing.')#为什么这里用单引号也能解释
 print("i'd much rather you 'not'.")
 print('I "said" do not touch this.')
 
