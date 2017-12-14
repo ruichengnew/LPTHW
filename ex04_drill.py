a7ac8599d9d08c0236aad0c4ecc7ec3c81c64347
程瑞 <chengrui_tfc@163.com>
2017-12-14 09:30:36 +0800

    Add files via upload

---
+++ ex04_drill.py
@@ -0,0 +1,15 @@
+#File "ex4.py", line 8, in <module>
+#"car_pool_capacity" is not defined, but "carpool_capacity".
+#Also "passenger" is not defined, but "passengers".
+
+#Question 1: No, it's not. If we use 4, then the carpool_capacity will be 120, but not 120.0.
+
+#Question 2: OK
+
+#Question 3: maybe each variable should be defined clearly and accurately.
+
+#Question 4: "=" is to give data(numbers, strings, etc) names.
+
+#Question 5: underscore chracter _
+
+#Question 6: let me try.
