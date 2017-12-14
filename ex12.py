01c7300b7f230cad0c86289f4c74e65ecd738cb2
程瑞 <chengrui_tfc@163.com>
2017-12-14 21:25:20 +0800

    Add files via upload

---
+++ ex12.py
@@ -0,0 +1,6 @@
+age = input("How old are you?\t")
+height = input("How tall are you?\t")
+weight = input("How much do you weigh?\t")
+
+print(f"So, you are {age} old, {height} tall and {weight} heavy.")
+#这个input很好玩，但是下面突如起来的pydoc让我有点慌，下一节
