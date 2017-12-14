88dec3df6635c2946aac96083c9bf1c14387a093
程瑞 <chengrui_tfc@163.com>
2017-12-14 15:17:01 +0800

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months) #教给我/n的用法，语句中/n意味着换一行
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""") #教给我three quotes的用法，用三引号，则可以打印尽可能多的东西
