94bde41ad172440f4180201e5bf90ad922d1bd6f
程瑞 <chengrui_tfc@163.com>
2017-12-14 10:03:49 +0800

    Add files via upload

---
+++ ex05_drill.py
@@ -0,0 +1,10 @@
+#Question 1: useless
+
+#Question 2
+inches = 74
+lbs = 180
+centimeters = inches*2.54
+kilogrames = lbs*0.45
+
+print(f"He's {centimeters} centimeters tall.")
+print(f"He's {kilogrames} kilogrames heavy.")
