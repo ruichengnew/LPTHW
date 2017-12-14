d974035ff57d124597f9b9a6182b08b0968e9441
程瑞 <chengrui_tfc@163.com>
2017-12-14 15:16:43 +0800

    Add files via upload

---
+++ ex8.py
@@ -0,0 +1,13 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, True, False))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+    "Try your",
+    "Own text here",
+    "Maybe a poem",
+    "Or a song about fear"
+))
+#在目前学习的范围内，就是将数学的函数迭代思想反复的运用，而且任何一点小的的错误都不能犯，不然就bug
