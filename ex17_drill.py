394b6da869986fb3d460ce6f3398532680171d12
程瑞 <chengrui_tfc@163.com>
2017-12-15 15:17:42 +0800

    Add files via upload

---
+++ ex17_drill.py
@@ -0,0 +1,14 @@
+#Question 1
+from sys import argv
+script, from_file, to_file = argv
+open(to_file, 'w').write(open(from_file).read())
+#I think this is the "one line long" script
+
+#Question 3
+#Old command, I will learn Command line crash course later carefully later.
+
+#Question 4
+#Sorry, I delete the last two lines and run the script, everything is alright.
+#So I can't find the reason.
+#But I guess, if I don't close them, the PowerShell will always remember I opened a file, and take bits of CPU, maybe.
+    
