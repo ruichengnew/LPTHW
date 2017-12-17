23a83bd478536c7b72f5e7e35afb5db393677fd8
程瑞 <chengrui_tfc@163.com>
2017-12-17 22:40:05 +0800

    Add files via upload

---
+++ ex25_drill.py
@@ -0,0 +1,14 @@
+#Question 1
+#后面的函数是几个复合函数，都是先break再sort在打印的，小trick而已
+
+#Question 2
+#"""三引号这种东西，跟#很像，但是看起来能够被Python读取，并且识别到某个被称之为help的库里，当你想要的时候，就直接提取出来了
+#documentation comments，果然跟comment差不多
+
+#Question 3
+#from ex25 import *
+#import ex25
+#一些小东西，这本书都是在各种莫名的地方教给你，要么就是什么都不说等你去查，这种教学方式有点意思，回到本书的开头，这本书并不适合所有人，别强求。
+
+#Question 4
+#quit()
