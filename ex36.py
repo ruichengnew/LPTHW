feea98db9ddd81042e709dec3b3cd03ae5232518
程瑞 <chengrui_tfc@163.com>
2017-12-17 22:42:03 +0800

    Add files via upload

---
+++ ex36.py
@@ -0,0 +1,9 @@
+"""
+1. On a sheet of paper or an index card, write a list of tasks you need to complete to finish the software. This is your to do list.
+2. Pick the easoest thing you can do from your list.
+3. write out English comments in your source file as a guide for how you would accomplish this task in your code.
+4. Write some of the code under the English comments.
+5. Quickly run your script so see if that code worked.
+6. Keep working in a cycle of writing some code, running it to test it, and fixing it until it works.
+7. Cross this taks off your list, then pick your next easiest task and repeat.
+"""
