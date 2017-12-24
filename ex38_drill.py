a0a945c1d246dab191f1644f14457eca76335ab1
程瑞 <chengrui_tfc@163.com>
2017-12-24 12:44:08 +0800

    Add files via upload

---
+++ ex38_drill.py
@@ -0,0 +1,27 @@
+#Question 1
+#See in ex38
+
+#Question 2
+#Both of them call pop, one is on more_stuff, the other is with argument more_stuff, and at this time, they are the same.
+
+#Question 3
+#Wikipedia
+
+#Question 4
+#PYTHON Documentation
+#终于理解什么是python documentation。。。真的长。。
+
+#Question 5
+#OOP，OK，I will figure you out!
+
+#Question 6
+print("I want to make ten friends here, but now I only have four. Wuzong, Lawrence, Kunge, Wendy")
+ten_friends = "Wuzong Lawrence Kunge Wendy"
+trying = ten_friends.split(' ')
+
+for i in range(6):
+    friend = input("Many things happened around me. And you came to me. Thanks,  " )
+    trying.append(friend)
+
+print("Ok, now my dream comes true. My friends are:\n")
+print(trying)
