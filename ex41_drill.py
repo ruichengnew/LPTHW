40fb211fe9e58b19494ee7eeb7289efaea7af4b1
程瑞 <chengrui_tfc@163.com>
2017-12-24 12:45:00 +0800

    Add files via upload

---
+++ ex41_drill.py
@@ -0,0 +1,4 @@
+#1. For each class give its name and what other classes it inherits fromself.
+#2. Under that, list every function it has and the parameters they takeself.
+#3. List all of the attributes it uses on its self.
+#4. For each attribute, give the class this attribute is.
