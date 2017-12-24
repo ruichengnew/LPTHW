08d55b554f971e138f398534e8e1e3a60f77f00e
程瑞 <chengrui_tfc@163.com>
2017-12-24 12:40:54 +0800

    Add files via upload

---
+++ ex28_drill.py
@@ -0,0 +1,27 @@
+#Question 1
+#让一个初学者去整理， 这个作者也是心大， 我不知道有多少个等号，等碰到了再说
+
+#Question 2
+# ！=    not equal
+# ==    equal
+# +=    equal and plus itself
+# >=    greater than or equal to
+# <=    less than or equal to
+#That's all.
+
+#Question 3
+#想感觉还是要想一下把。。瞬间反应真的难，不过就表格中的内容，瞬间反应就还可以
+
+#Question 4
+
+#Common Student Questions
+第一个！
+这个问题提的很好，把我的疑问讲出来了，并且这个作者的回答并没有解决问题，那么我就查了下，
+第一次使用StackOverflow得到结果，在Boolean Logic中，0 == False， Nonzero == Ture，所以有以下返回值
+0 == False -> True
+1 == True -> True
+1 and 0 -> 0
+1 or 0 -> 1
+not (1 and 0) -> True
+not (1 or 0) -> False
+我只列出了一些简单的例子，不过这个举例告诉我不懂就问StackOverflow！
