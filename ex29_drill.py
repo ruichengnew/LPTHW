ec3b19ca9343ec70fa7f36657997fc11927064ad
程瑞 <chengrui_tfc@163.com>
2017-12-24 12:41:26 +0800

    Add files via upload

---
+++ ex29_drill.py
@@ -0,0 +1,23 @@
+#Question 1
+#IF 后面跟着的是条件，如果条件满足，就执行下面的指令，如果不满足，就跳过指令
+#it lets you make decision in your Python code, we can test the truth of a variable and based on the truth , run a piece of code or not run it
+
+#Quesiton 2
+#It is a kind of format, this part of code will be decided by the truth of the variable after if.
+
+#Question 3
+#it won't be decided, just be run directly
+
+#Question 4
+#OK， let me do three
+if 1 == 1:
+    print("I'm a genius!")
+
+if 1 == 0:
+    print("I'm a foolist.")
+
+if "山无棱，天地合" or 1:
+    print("We will be together till forever.")
+
+#Question 5
+#do different code
