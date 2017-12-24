0c72a1b3e72c5acb08f12099ea9f1dfa1c1eb185
程瑞 <chengrui_tfc@163.com>
2017-12-24 12:42:43 +0800

    Add files via upload

---
+++ ex32_drill.py
@@ -0,0 +1,21 @@
+#Question 1
+for i in range(1,5):
+    print(i, end = ' ')
+for i in range(5):
+    print(i)
+for i in range(0,7,3):
+    print(i)
+# maybe know a little about range()
+
+#Question 2
+elements = range(0,6)
+for i in elements:
+    print(i)
+
+#Question 3
+#many things, such as
+delitem
+getitem
+copy
+counts
+#但是我并不会用啊。。。这本书在这个方面真是。。有点让人苦恼，什么都要我自己打出来然后run
